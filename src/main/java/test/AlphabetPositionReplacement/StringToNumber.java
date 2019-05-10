7ba6de813d71c3c7e884adaf0944739967d3e301
Kiregor <kiregorekal@gmail.com>
2019-05-10 15:47:40 +0100

    Code to meet the ASCII code test

--- src/main/java/test/AlphabetPositionReplacement/StringToNumber.java
+++ src/main/java/test/AlphabetPositionReplacement/StringToNumber.java
@@ -1,9 +1,15 @@
 package test.AlphabetPositionReplacement;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class StringToNumber {
 
 	public static String stringToLower(String in) {
 		return in.toLowerCase();
 	}
 	
+	public static List<Integer> stringToASCIIvalues(String in){
+		return in.toLowerCase().chars().mapToObj(c -> (int) c).collect(Collectors.toList());
+	}
 }
