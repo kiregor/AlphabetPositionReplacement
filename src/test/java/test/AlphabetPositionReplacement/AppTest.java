1b2b6d2a9b4fd89d04e7ca2a1fc023a36c614baa
Kiregor <kiregorekal@gmail.com>
2019-05-10 14:46:42 +0100

    first commit

---
+++ src/test/java/test/AlphabetPositionReplacement/AppTest.java
@@ -0,0 +1,20 @@
+package test.AlphabetPositionReplacement;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.ArrayList;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase {
+	
+	public String stringToLower(String in) {
+		return in.toLowerCase();
+	}
+	
+}
