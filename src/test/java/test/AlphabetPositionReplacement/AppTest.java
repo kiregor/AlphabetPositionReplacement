332ecaed5151a4780a2060882aaf26e61b85a417
Kiregor <kiregorekal@gmail.com>
2019-05-10 15:45:37 +0100

    Created test for the ASCII values of the whole string

--- src/test/java/test/AlphabetPositionReplacement/AppTest.java
+++ src/test/java/test/AlphabetPositionReplacement/AppTest.java
@@ -19,4 +19,14 @@ public class AppTest extends TestCase {
 		assertEquals(expected, test);
 	}
 	
+	public void testStringToASCIIvalues() {
+		int[] arr = {116, 104, 105, 115, 32, 110, 101, 116, 98, 117, 105, 108, 100, 101, 114, 32, 97, 115, 115, 101, 115, 115, 109, 101, 110, 116, 32, 105, 115, 32, 119, 97, 121, 32, 116, 111, 111, 32, 101, 97, 115, 121, 46};
+		List<Integer> exp = new ArrayList<>(arr.length);
+		for(int i : arr) {
+			exp.add(Integer.valueOf(i));
+		}
+		List<Integer> test = StringToNumber.stringToASCIIvalues("This NETbuilder assessment is way too easy.");
+		assertEquals(exp, test);
+	}
+	
 }
