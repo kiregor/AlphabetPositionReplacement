602286734e8ad1dd1de7c73dc8985a0186e31a3b
Kiregor <kiregorekal@gmail.com>
2019-05-11 15:30:37 +0100

    Added a front end to make it easy to use

--- src/main/java/test/AlphabetPositionReplacement/App.java
+++ src/main/java/test/AlphabetPositionReplacement/App.java
@@ -1,13 +1,40 @@
 package test.AlphabetPositionReplacement;
 
-/**
- * Hello world!
- *
- */
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
 public class App 
 {
+	JFrame window;
+	
+	public App() {
+		window = new JFrame("Characters to Numbers");
+		window.setLayout(new FlowLayout());
+		JTextField tboxinput = new JTextField(25);
+		JTextField tboxoutput = new JTextField(25);
+		JButton button = new JButton("Transform");
+		
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				tboxoutput.setText(StringToNumber.stringToNumStringNoPunc(tboxinput.getText()));
+			}
+		});
+		
+		window.add(tboxinput);
+		window.add(button);
+		window.add(tboxoutput);
+		
+		window.setSize(350, 150);
+		window.setVisible(true);
+	}
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        new App();
     }
 }
